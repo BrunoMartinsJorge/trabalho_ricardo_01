2c3946fd883fd4f2d627612350fb0e4d563fa87d
Bruno <rexon300008@gmail.com>
2025-06-06 20:06:44 -0300

    feat: Adicionar a 1ª Questão - Calculadora.

---
+++ src/main/java/br/com/seguranca/questao_01/Calculadora.java
@@ -0,0 +1,25 @@
+package br.com.seguranca.questao_01;
+
+import lombok.Data;
+
+@Data
+public class Calculadora {
+
+    public float somar(float a, float b) {
+        return a + b;
+    }
+
+    public float subtrair(float a, float b) {
+        return a - b;
+    }
+
+    public float multiplicar(float a, float b) {
+        return a * b;
+    }
+
+    public float dividir(float a, float b) {
+        return a / b;
+    }
+
+    public Calculadora() { }
+}
