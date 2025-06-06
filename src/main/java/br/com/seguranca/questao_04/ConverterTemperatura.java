6bcca8e0f02a967ce910010d9fbe0ccb4f39e794
Bruno <rexon300008@gmail.com>
2025-06-06 20:59:06 -0300

    feat: Adicionar Questão 4 - Converter Temperatura.

---
+++ src/main/java/br/com/seguranca/questao_04/ConverterTemperatura.java
@@ -0,0 +1,14 @@
+package br.com.seguranca.questao_04;
+
+public class ConverterTemperatura {
+
+    public float coverterTemperaturaParaF(float temperatura){
+        return (temperatura * 9/5) + 32;
+    }
+
+    public float coverterTemperaturaParaC(float temperatura){
+        return (temperatura - 32) * 5/9;
+    }
+    //Celsius → Fahrenheit: F = (C * 9/5) + 32
+    //Fahrenheit → Celsius: C = (F - 32) * 5/9
+}
